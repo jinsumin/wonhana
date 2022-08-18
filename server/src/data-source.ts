4fc47931abcd58a7fbaa21e48afecbf84a99baeb
jinsumin <suminjin@naver.com>
2022-08-18 09:27:21 +0900

    postgres, typeorm

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,17 +1,16 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "password",
+  database: "postgres",
+  synchronize: true,
+  logging: false,
+  entities: ["src/entities/**/*.ts"],
+  migrations: [],
+  subscribers: [],
+});
