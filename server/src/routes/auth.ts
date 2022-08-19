a6f448688557b68a8d304d09c97ea4575a5b1d52
jinsumin <suminjin@naver.com>
2022-08-19 11:36:02 +0900

    register validation

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,9 +1,36 @@
+import { validate } from "class-validator";
 import { Router, Response, Request } from "express";
+import { User } from "../entities/User";
 
 const register = async (req: Request, res: Response) => {
   const { email, username, password } = req.body;
-  
-  
+
+  try {
+    let errors: any = {};
+    // 사용중인 정보인지 체크
+    const emailUser = await User.findOneBy({ email });
+    const usernameUser = await User.findOneBy({ username });
+
+    if (emailUser) errors.email = "사용중인 이메일 입니다.";
+    if (usernameUser) errors.username = "사용중인 사용자이름입니다.";
+
+    if (Object.keys(errors).length > 0) {
+      return res.status(400).json(errors);
+    }
+
+    const user = new User();
+    user.email = email;
+    user.username = username;
+    user.password = password;
+
+    errors = await validate(user);
+
+    await user.save();
+    return res.json(user);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error });
+  }
 };
 
 const router = Router();
