95fdfd3b3bda540093ab849688f3a4172d26705e
jinsumin <suminjin@naver.com>
2022-08-19 15:22:09 +0900

    login cookie

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,33 +1,35 @@
 import express from "express";
 import morgan from "morgan";
 import { AppDataSource } from "./data-source";
+
 import authRoutes from "./routes/auth";
+
 import cors from "cors";
 import dotenv from "dotenv";
 
 const app = express();
-const origin = "http://localhost:3000";
-
+const origin = process.env.ORIGIN;
 app.use(
   cors({
     origin,
-    crendentials: true,
+    credentials: true,
   })
 );
 app.use(express.json());
 app.use(morgan("dev"));
-
 dotenv.config();
 
 app.get("/", (_, res) => res.send("running"));
 app.use("/api/auth", authRoutes);
 
+app.use(express.static("public"));
+
 let port = 4000;
 app.listen(port, async () => {
-  console.log(`server running at http://localhost:${port}`);
+  console.log(`server running at ${process.env.APP_URL}`);
 
   AppDataSource.initialize()
-    .then(async () => {
+    .then(() => {
       console.log("database initialized");
     })
     .catch((error) => console.log(error));
