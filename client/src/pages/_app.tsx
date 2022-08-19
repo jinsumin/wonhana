ada9ac96c472fd2195f457e15e014021c986f9d1
jinsumin <suminjin@naver.com>
2022-08-19 15:50:36 +0900

    cors error : credentials true

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -4,6 +4,7 @@ import Axios from "axios";
 
 function MyApp({ Component, pageProps }: AppProps) {
   Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api";
+  Axios.defaults.withCredentials = true;
   return <Component {...pageProps} />;
 }
 
--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,67 +1,68 @@
-import Link from "next/link";
 import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React, { FormEvent, useState } from "react";
 import InputGroup from "../components/InputGroup";
-import { useRouter } from "next/router";
+// import { useAuthState } from "../context/auth";
 
 const Register = () => {
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState<any>({});
+  // const { authenticated } = useAuthState();
 
   let router = useRouter();
+  // if (authenticated) router.push("/");
 
   const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
-
     try {
-      const response = await axios.post("/auth/register", {
+      const res = await axios.post("/auth/register", {
         email,
         password,
         username,
       });
-
-      console.log("response : ", response);
+      console.log("res", res);
       router.push("/login");
     } catch (error: any) {
-      console.error(error);
-      setErrors(error?.response?.data || {});
+      console.log("error", error);
+      setErrors(error.response.data || {});
     }
   };
 
   return (
     <div className="bg-white">
-      <div className="flex flex-col items-center justify-content h-screen p-6">
+      <div className="flex flex-col items-center justify-center h-screen p-6">
         <div className="w-10/12 mx-auto md:w-96">
-          <div className="mb-2 text-lg font-medium">회원가입</div>
+          <h1 className="mb-2 text-lg font-medium">회원가입</h1>
           <form onSubmit={handleSubmit}>
             <InputGroup
-              placeholder="이메일을 입력하세요."
+              placeholder="Email"
               value={email}
               setValue={setEmail}
               error={errors.email}
             />
             <InputGroup
-              placeholder="사용자명을 입력하세요."
+              placeholder="Username"
               value={username}
               setValue={setUsername}
               error={errors.username}
             />
             <InputGroup
-              placeholder="비밀번호를 입력하세요."
+              placeholder="Password"
               value={password}
               setValue={setPassword}
               error={errors.password}
             />
-            <button className="w-full h-12 py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded">
-              회원가입
+            <button className="w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded">
+              회원 가입
             </button>
           </form>
           <small>
             이미 가입하셨나요?
             <Link href="/login">
-              <a className="ml-1 text-blue-600 uppercase">로그인</a>
+              <a className="ml-1 text-blue-500 uppercase">로그인</a>
             </Link>
           </small>
         </div>
