0b5526e9e0086621122df10c631c037e333311f1
jinsumin <suminjin@naver.com>
2022-08-26 10:37:30 +0900

    get post comments

--- server/src/routes/posts.ts
+++ server/src/routes/posts.ts
@@ -53,6 +53,25 @@ const createPost = async (req: Request, res: Response) => {
   }
 };
 
+const getPostComments = async (req: Request, res: Response) => {
+  const { identifier, slug } = req.params;
+  try {
+    const post = await Post.findOneByOrFail({ identifier, slug });
+    const commnets = await Comment.find({
+      where: { postId: post.id },
+      order: { createdAt: "DESC" },
+      relations: ["votes"],
+    });
+    if (res.locals.user) {
+      commnets.forEach((e) => e.setUserVote(res.locals.user));
+    }
+    return res.json(commnets);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "문제가 발생하였습니다." });
+  }
+};
+
 const createPostComment = async (req: Request, res: Response) => {
   const { identifier, slug } = req.params;
   const body = req.body.body;
@@ -74,8 +93,11 @@ const createPostComment = async (req: Request, res: Response) => {
 };
 
 const router = Router();
+
 router.get("/:identifier/:slug", userMiddleware, getPost);
 router.post("/", userMiddleware, authMiddleware, createPost);
+
+router.get("/:identifier/:slug/comments", userMiddleware, getPostComments);
 router.post("/:identifier/:slug/comments", userMiddleware, createPostComment);
 
 export default router;
